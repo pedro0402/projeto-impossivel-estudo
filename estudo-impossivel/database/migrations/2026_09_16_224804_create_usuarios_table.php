783dbde8c74b6f793fa9e80d24f7897d2b0d51ee
Pedro Moraes <pedro693.ph@gmail.com>
2026-09-16 20:05:35 -0300

    feat: criação do model de Usuario

---
+++ estudo-impossivel/database/migrations/2026_09_16_224804_create_usuarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('email', 150)->unique();
+            $table->string('senha', 255);
+            $table->text('endereco')->nullable();
+            $table->string('perfil', 25)->nullable(false)->default('comum');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
