4e85b10528e8e5a6702e1a1ee4cbb300746e792c
Pedro Moraes <pedro693.ph@gmail.com>
2026-09-16 23:38:40 -0300

    feat: criação das variaveis no model de usuario que permitem os campos serem preenchidos e tambem para esconder a senha ao retornar em json

--- estudo-impossivel/app/Models/Usuario.php
+++ estudo-impossivel/app/Models/Usuario.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    //quais campos podem ser preenchidos via create()/fill()
+    protected $fillable = [
+        'nome',
+        'email',
+        'senha',
+        'endereco',
+        'perfil'
+    ];
+
+    //quais campos não devem aparecer quando o Model é convertido para array/JSON
+    protected $hidden = [
+        'senha'
+    ];
 }
