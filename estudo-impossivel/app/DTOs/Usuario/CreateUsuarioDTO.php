63dc8c869c3fb0603eaecb4c03dfaa12662f8125
Pedro Moraes <pedro693.ph@gmail.com>
2026-09-17 00:16:44 -0300

    feat: criação de dto para usuario

---
+++ estudo-impossivel/app/DTOs/Usuario/CreateUsuarioDTO.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DTOs\Usuario;
+
+class CreateUsuarioDTO 
+{
+    readonly string $nome;
+    readonly string $email;
+    readonly string $senha;
+    readonly ?string $endereco;
+    readonly string $perfil;
+
+    public function __construct(string $nome, string $email, string $senha, ?string $endereco, string $perfil)
+    {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->senha = $senha;
+        $this->endereco = $endereco;
+        $this->perfil = $perfil; 
+
+    }
+    
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome():string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail():string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get the value of senha
+     */ 
+    public function getSenha():string
+    {
+        return $this->senha;
+    }
+
+    /**
+     * Get the value of endereco
+     */ 
+    public function getEndereco():?string
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * Get the value of perfil
+     */ 
+    public function getPerfil():string
+    {
+        return $this->perfil;
+    }
+}
